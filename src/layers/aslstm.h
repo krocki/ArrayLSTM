50de2edbed86e1e9e910ca7357fe3a87ea045bc5
krocki <kmrocki@us.ibm.com>
2016-07-08 22:21:31 -0700

    added headers

--- src/layers/aslstm.h
+++ src/layers/aslstm.h
@@ -1,12 +1,26 @@
 /*
-*   @Author: kmrocki
-*   @Date:   2016-02-19 15:25:43
-*   @Last Modified by:   kmrocki
-*   @Last Modified time: 2016-03-24 18:36:57
-*
-*   Implementation of asLSTM layer
-*
-*/
+ *
+ * Author: Kamil Rocki <kmrocki@us.ibm.com>
+ *
+ * Copyright (c) 2016, IBM Corporation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ * Implementation of asLSTM layer
+ *
+ */
 
 #ifndef _asLSTM_H_
 #define _asLSTM_H_
@@ -33,157 +47,157 @@
 template <typename T>
 class asLSTM : public Timelayer<T> {
 
-public:
-
-	using Timelayer<T>::s;
-	T rands;
-
-	size_t L = 1; // memory cells per hidden neuron
-
-	/* main constructor */
-	asLSTM ( size_t _M, size_t _N, size_t _B,
-			 size_t _S, size_t _L = 1 ) : L ( _L ),
-
-		Timelayer<T> ( _M, _N, _B, _S,
-
-	{	"aslstm"		},
-
-	{
-		/* define states */
-		std::make_tuple ( "h", _B, _N ),
-		std::make_tuple ( "max_o", _B, _N ),
-		std::make_tuple ( "c", _B, _N * _L ),
-		std::make_tuple ( "g", _B, 4 * _N * 1 ),
-		std::make_tuple ( "g2", _B, 4 * _N * 1 )
-
-	}, {
-
-		/* define params */
-		std::make_tuple ( "W", _M, 4 * _N * 1 ),
-		std::make_tuple ( "U", _N, 4 * _N * 1 ),
-		std::make_tuple ( "b", 1, 4 * _N * 1 ),
-
-		std::make_tuple ( "B_ones", _B, 1 )
-
-	} ) {
-
-		std::cout << "aslstm levels :" << _L << std::endl;
-		/*init*/
-		matrix_init ( p ( W ) );
-		matrix_init ( p ( U ) );
-
-		rands = T ( _B, _N );
-
-		// set biases of f gates to 1
-		// (http://jmlr.org/proceedings/papers/v37/jozefowicz15.pdf)
-		p ( b ).block_forall ( 2 * N * 1, N * 1, [ = ] () { return 1; } );
-		//p ( b ).block_forall ( 2 * N * L, N, [ = ] () { return 1; } );
-		p ( B_ones ).forall ( [ = ] () { return 1; } );
-
-		p ( B_ones ).sync_device();
-
-	}
-
-	~asLSTM() {
-
-
-	}
-
+	public:
+	
+		using Timelayer<T>::s;
+		T rands;
+		
+		size_t L = 1; // memory cells per hidden neuron
+		
+		/* main constructor */
+		asLSTM ( size_t _M, size_t _N, size_t _B,
+				 size_t _S, size_t _L = 1 ) : L ( _L ),
+				 
+			Timelayer<T> ( _M, _N, _B, _S,
+			
+		{	"aslstm"		},
+		
+		{
+			/* define states */
+			std::make_tuple ( "h", _B, _N ),
+			std::make_tuple ( "max_o", _B, _N ),
+			std::make_tuple ( "c", _B, _N * _L ),
+			std::make_tuple ( "g", _B, 4 * _N * 1 ),
+			std::make_tuple ( "g2", _B, 4 * _N * 1 )
+			
+		}, {
+		
+			/* define params */
+			std::make_tuple ( "W", _M, 4 * _N * 1 ),
+			std::make_tuple ( "U", _N, 4 * _N * 1 ),
+			std::make_tuple ( "b", 1, 4 * _N * 1 ),
+			
+			std::make_tuple ( "B_ones", _B, 1 )
+			
+		} ) {
+		
+			std::cout << "aslstm levels :" << _L << std::endl;
+			/*init*/
+			matrix_init ( p ( W ) );
+			matrix_init ( p ( U ) );
+			
+			rands = T ( _B, _N );
+			
+			// set biases of f gates to 1
+			// (http://jmlr.org/proceedings/papers/v37/jozefowicz15.pdf)
+			p ( b ).block_forall ( 2 * N * 1, N * 1, [ = ] () { return 1; } );
+			//p ( b ).block_forall ( 2 * N * L, N, [ = ] () { return 1; } );
+			p ( B_ones ).forall ( [ = ] () { return 1; } );
+			
+			p ( B_ones ).sync_device();
+			
+		}
+		
+		~asLSTM() {
+		
+		
+		}
+		
 #define c_gates 3 * N * L * s(t, g).rows()
 #define i_gates 0 * N * L * s(t, g).rows()
 #define f_gates 2 * N * L * s(t, g).rows()
 #define o_gates 1 * N * L * s(t, g).rows()
-
-	virtual void forward ( bool apply_dropout, size_t t = 1 ) {
-
-		s ( t, x ).sync_device();
-		s ( t - 1, h ).sync_device();
-		s ( t - 1, c ).sync_device();
-
-		// put in 2 separate streams
-		cublasSetStream ( handle, streams[1] );
-		CU_GEMM ( s ( t, g ), s ( t, x ), p ( W ), false, false, 1, 0 );
-
-		cublasSetStream ( handle, streams[2] );
-		CU_GEMM ( s ( t, g2 ), s ( t - 1, h ), p ( U ), false, false, 1, 0 );
-
-		sync_stream ( 1 );
-		sync_stream ( 2 );
-
-		cu_rand ( rands.cu_data, rands.size() );
-
-		//fused
-		cu_elementwise_aslstm_forward (	& ( s ( t, g ).cu_data[0] ),
-										& ( s ( t, g2 ).cu_data[0] ),
-										& ( p ( b ).cu_data[0] ),
-										& ( s ( t, h ).cu_data[0] ),
-										& ( s ( t, max_o ).cu_data[0] ),
-										& ( s ( t, c ).cu_data[0] ),
-										& ( s ( t - 1, c ).cu_data[0] ),
-										& ( rands.cu_data[0] ),
-										N, L, s ( t, c ).rows() );
-
-		s ( t, c ).sync_host();
-		s ( t, h ).sync_host();
-
-	}
-
-	virtual void backward ( bool apply_dropout, size_t t ) {
-
-		// error coming from higher layers: dh[t] = dy[t]
-
-		cu_elementwise_aslstm_backward (	& ( g ( t, g ).cu_data[0] ),
-											& ( g ( t, y ).cu_data[0] ),
-											& ( s ( t, c ).cu_data[0] ),
-											& ( g ( t, c ).cu_data[0] ),
-											& ( s ( t, g ).cu_data[0] ),
-											& ( s ( t - 1, c ).cu_data[0] ),
-											& ( g ( t - 1, c ).cu_data[0] ),
-											s ( t, h ).cu_data,
+		
+		virtual void forward ( bool apply_dropout, size_t t = 1 ) {
+		
+			s ( t, x ).sync_device();
+			s ( t - 1, h ).sync_device();
+			s ( t - 1, c ).sync_device();
+			
+			// put in 2 separate streams
+			cublasSetStream ( handle, streams[1] );
+			CU_GEMM ( s ( t, g ), s ( t, x ), p ( W ), false, false, 1, 0 );
+			
+			cublasSetStream ( handle, streams[2] );
+			CU_GEMM ( s ( t, g2 ), s ( t - 1, h ), p ( U ), false, false, 1, 0 );
+			
+			sync_stream ( 1 );
+			sync_stream ( 2 );
+			
+			cu_rand ( rands.cu_data, rands.size() );
+			
+			//fused
+			cu_elementwise_aslstm_forward (	& ( s ( t, g ).cu_data[0] ),
+											& ( s ( t, g2 ).cu_data[0] ),
+											& ( p ( b ).cu_data[0] ),
+											& ( s ( t, h ).cu_data[0] ),
 											& ( s ( t, max_o ).cu_data[0] ),
-											N, L, g ( t, c ).rows() );
-
-		//backprop through linear part (forward pass step 1)
-		//these are computed in parallel
-		cublasSetStream ( handle, streams[1] );
-		CU_GEMM ( d ( b ), p ( B_ones ), g ( t, g ), true, false );
-
-		cublasSetStream ( handle, streams[2] );
-		CU_GEMM ( d ( U ), s ( t - 1, h ), g ( t, g ), true, false );
-
-		cublasSetStream ( handle, streams[3] );
-		CU_GEMM ( d ( W ), s ( t, x ), g ( t, g ), true, false );
-
-		cublasSetStream ( handle, streams[4] );
-		//backprop into inputs for lower layers
-		CU_GEMM ( g ( t, x ), g ( t, g ), p ( W ), false, true );
-
-		cublasSetStream ( handle, streams[5] );
-		//carry - h state
-		CU_GEMM ( g ( t - 1, y ), g ( t, g ), p ( U ), false , true );
-
-		sync_stream ( 1 );
-		sync_stream ( 2 );
-		sync_stream ( 3 );
-		sync_stream ( 4 );
-		sync_stream ( 5 );
-	}
-
-	virtual void reset ( dtype std ) {
-
-		randn ( s ( 0, h ), ( dtype ) 0, ( dtype ) std );
-		randn ( s ( 0, c ), ( dtype ) 0, ( dtype ) std );
-		// s ( 0, h ).cu_zero();
-		// s ( 0, c ).cu_zero();
-	}
-
-	/* optional */
-	/* copy constr */
-	//asLSTM( const asLSTM& src) : Timelayer(src.M, src.N, src.B, src.S)  { }
-
-	/* assignent operator */
-	//asLSTM& operator=( const asLSTM& src) { Timelayer::operator=(src); return *this; }
-
+											& ( s ( t, c ).cu_data[0] ),
+											& ( s ( t - 1, c ).cu_data[0] ),
+											& ( rands.cu_data[0] ),
+											N, L, s ( t, c ).rows() );
+											
+			s ( t, c ).sync_host();
+			s ( t, h ).sync_host();
+			
+		}
+		
+		virtual void backward ( bool apply_dropout, size_t t ) {
+		
+			// error coming from higher layers: dh[t] = dy[t]
+			
+			cu_elementwise_aslstm_backward (	& ( g ( t, g ).cu_data[0] ),
+												& ( g ( t, y ).cu_data[0] ),
+												& ( s ( t, c ).cu_data[0] ),
+												& ( g ( t, c ).cu_data[0] ),
+												& ( s ( t, g ).cu_data[0] ),
+												& ( s ( t - 1, c ).cu_data[0] ),
+												& ( g ( t - 1, c ).cu_data[0] ),
+												s ( t, h ).cu_data,
+												& ( s ( t, max_o ).cu_data[0] ),
+												N, L, g ( t, c ).rows() );
+												
+			//backprop through linear part (forward pass step 1)
+			//these are computed in parallel
+			cublasSetStream ( handle, streams[1] );
+			CU_GEMM ( d ( b ), p ( B_ones ), g ( t, g ), true, false );
+			
+			cublasSetStream ( handle, streams[2] );
+			CU_GEMM ( d ( U ), s ( t - 1, h ), g ( t, g ), true, false );
+			
+			cublasSetStream ( handle, streams[3] );
+			CU_GEMM ( d ( W ), s ( t, x ), g ( t, g ), true, false );
+			
+			cublasSetStream ( handle, streams[4] );
+			//backprop into inputs for lower layers
+			CU_GEMM ( g ( t, x ), g ( t, g ), p ( W ), false, true );
+			
+			cublasSetStream ( handle, streams[5] );
+			//carry - h state
+			CU_GEMM ( g ( t - 1, y ), g ( t, g ), p ( U ), false , true );
+			
+			sync_stream ( 1 );
+			sync_stream ( 2 );
+			sync_stream ( 3 );
+			sync_stream ( 4 );
+			sync_stream ( 5 );
+		}
+		
+		virtual void reset ( dtype std ) {
+		
+			randn ( s ( 0, h ), ( dtype ) 0, ( dtype ) std );
+			randn ( s ( 0, c ), ( dtype ) 0, ( dtype ) std );
+			// s ( 0, h ).cu_zero();
+			// s ( 0, c ).cu_zero();
+		}
+		
+		/* optional */
+		/* copy constr */
+		//asLSTM( const asLSTM& src) : Timelayer(src.M, src.N, src.B, src.S)  { }
+		
+		/* assignent operator */
+		//asLSTM& operator=( const asLSTM& src) { Timelayer::operator=(src); return *this; }
+		
 };
 
 #undef N
--- src/layers/hhlstm.h
+++ src/layers/hhlstm.h
@@ -1,12 +1,25 @@
 /*
-*   @Author: kmrocki
-*   @Date:   2016-02-19 15:25:43
-*   @Last Modified by:   kmrocki
-*   @Last Modified time: 2016-03-24 18:36:57
-*
-*   Implementation of hhLSTM layer
-*
-*/
+ *
+ * Author: Kamil Rocki <kmrocki@us.ibm.com>
+ *
+ * Copyright (c) 2016, IBM Corporation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ *
+ */
 
 #ifndef _hhLSTM_H_
 #define _hhLSTM_H_
@@ -33,148 +46,148 @@
 template <typename T>
 class hhLSTM : public Timelayer<T> {
 
-public:
-
-	using Timelayer<T>::s;
-
-	size_t L = 1; // memory cells per hidden neuron
-
-	/* main constructor */
-	hhLSTM ( size_t _M, size_t _N, size_t _B,
-			 size_t _S, size_t _L = 1 ) : L ( _L ),
-
-		Timelayer<T> ( _M, _N, _B, _S,
-
-	{	"hhlstm"		},
-
-	{
-		/* define states */
-		std::make_tuple ( "h", _B, _N ),
-		std::make_tuple ( "c", _B, _N * _L ),
-		std::make_tuple ( "g", _B, 4 * _N * 1 ),
-		std::make_tuple ( "g2", _B, 4 * _N * 1 )
-
-	}, {
-
-		/* define params */
-		std::make_tuple ( "W", _M, 4 * _N * 1 ),
-		std::make_tuple ( "U", _N, 4 * _N * 1 ),
-		std::make_tuple ( "b", 1, 4 * _N * 1 ),
-
-		std::make_tuple ( "B_ones", _B, 1 )
-
-	} ) {
-
-		std::cout << "hhlstm levels :" << _L << std::endl;
-
-		/*init*/
-		matrix_init ( p ( W ) );
-		matrix_init ( p ( U ) );
-
-		// set biases of f gates to 1
-		// (http://jmlr.org/proceedings/papers/v37/jozefowicz15.pdf)
-		p ( b ).block_forall ( 2 * N * 1, N * 1, [ = ] () { return 1; } );
-		//p ( b ).block_forall ( 2 * N * L, N, [ = ] () { return 1; } );
-		p ( B_ones ).forall ( [ = ] () { return 1; } );
-
-		p ( B_ones ).sync_device();
-
-	}
-
-	~hhLSTM() {
-
-
-	}
-
+	public:
+	
+		using Timelayer<T>::s;
+		
+		size_t L = 1; // memory cells per hidden neuron
+		
+		/* main constructor */
+		hhLSTM ( size_t _M, size_t _N, size_t _B,
+				 size_t _S, size_t _L = 1 ) : L ( _L ),
+				 
+			Timelayer<T> ( _M, _N, _B, _S,
+			
+		{	"hhlstm"		},
+		
+		{
+			/* define states */
+			std::make_tuple ( "h", _B, _N ),
+			std::make_tuple ( "c", _B, _N * _L ),
+			std::make_tuple ( "g", _B, 4 * _N * 1 ),
+			std::make_tuple ( "g2", _B, 4 * _N * 1 )
+			
+		}, {
+		
+			/* define params */
+			std::make_tuple ( "W", _M, 4 * _N * 1 ),
+			std::make_tuple ( "U", _N, 4 * _N * 1 ),
+			std::make_tuple ( "b", 1, 4 * _N * 1 ),
+			
+			std::make_tuple ( "B_ones", _B, 1 )
+			
+		} ) {
+		
+			std::cout << "hhlstm levels :" << _L << std::endl;
+			
+			/*init*/
+			matrix_init ( p ( W ) );
+			matrix_init ( p ( U ) );
+			
+			// set biases of f gates to 1
+			// (http://jmlr.org/proceedings/papers/v37/jozefowicz15.pdf)
+			p ( b ).block_forall ( 2 * N * 1, N * 1, [ = ] () { return 1; } );
+			//p ( b ).block_forall ( 2 * N * L, N, [ = ] () { return 1; } );
+			p ( B_ones ).forall ( [ = ] () { return 1; } );
+			
+			p ( B_ones ).sync_device();
+			
+		}
+		
+		~hhLSTM() {
+		
+		
+		}
+		
 #define c_gates 3 * N * 1 * s(t, g).rows()
 #define i_gates 0 * N * 1 * s(t, g).rows()
 #define f_gates 2 * N * 1 * s(t, g).rows()
 #define o_gates 1 * N * 1 * s(t, g).rows()
-
-	virtual void forward ( bool apply_dropout, size_t t = 1 ) {
-
-		s ( t, x ).sync_device();
-		s ( t - 1, h ).sync_device();
-		s ( t - 1, c ).sync_device();
-
-		// put in 2 separate streams
-		cublasSetStream ( handle, streams[1] );
-		CU_GEMM ( s ( t, g ), s ( t, x ), p ( W ), false, false, 1, 0 );
-
-		cublasSetStream ( handle, streams[2] );
-		CU_GEMM ( s ( t, g2 ), s ( t - 1, h ), p ( U ), false, false, 1, 0 );
-
-		sync_stream ( 1 );
-		sync_stream ( 2 );
-		//fused
-		cu_elementwise_hhlstm_forward (	& ( s ( t, g ).cu_data[0] ),
-										& ( s ( t, g2 ).cu_data[0] ),
-										& ( p ( b ).cu_data[0] ),
-										& ( s ( t, h ).cu_data[0] ),
-										& ( s ( t, c ).cu_data[0] ),
-										& ( s ( t - 1, c ).cu_data[0] ),
-										N, L, s ( t, c ).rows() );
-
-		s ( t, c ).sync_host();
-		s ( t, h ).sync_host();
-
-	}
-
-	virtual void backward ( bool apply_dropout, size_t t ) {
-
-		// error coming from higher layers: dh[t] = dy[t]
-
-		cu_elementwise_hhlstm_backward (	& ( g ( t, g ).cu_data[0] ),
-											& ( g ( t, y ).cu_data[0] ),
+		
+		virtual void forward ( bool apply_dropout, size_t t = 1 ) {
+		
+			s ( t, x ).sync_device();
+			s ( t - 1, h ).sync_device();
+			s ( t - 1, c ).sync_device();
+			
+			// put in 2 separate streams
+			cublasSetStream ( handle, streams[1] );
+			CU_GEMM ( s ( t, g ), s ( t, x ), p ( W ), false, false, 1, 0 );
+			
+			cublasSetStream ( handle, streams[2] );
+			CU_GEMM ( s ( t, g2 ), s ( t - 1, h ), p ( U ), false, false, 1, 0 );
+			
+			sync_stream ( 1 );
+			sync_stream ( 2 );
+			//fused
+			cu_elementwise_hhlstm_forward (	& ( s ( t, g ).cu_data[0] ),
+											& ( s ( t, g2 ).cu_data[0] ),
+											& ( p ( b ).cu_data[0] ),
+											& ( s ( t, h ).cu_data[0] ),
 											& ( s ( t, c ).cu_data[0] ),
-											& ( g ( t, c ).cu_data[0] ),
-											& ( s ( t, g ).cu_data[0] ),
 											& ( s ( t - 1, c ).cu_data[0] ),
-											& ( g ( t - 1, c ).cu_data[0] ),
-											s ( t, h ).cu_data,
-											N, L, g ( t, c ).rows() );
-
-		//backprop through linear part (forward pass step 1)
-		//these are computed in parallel
-		cublasSetStream ( handle, streams[1] );
-		CU_GEMM ( d ( b ), p ( B_ones ), g ( t, g ), true, false );
-
-		cublasSetStream ( handle, streams[2] );
-		CU_GEMM ( d ( U ), s ( t - 1, h ), g ( t, g ), true, false );
-
-		cublasSetStream ( handle, streams[3] );
-		CU_GEMM ( d ( W ), s ( t, x ), g ( t, g ), true, false );
-
-		cublasSetStream ( handle, streams[4] );
-		//backprop into inputs for lower layers
-		CU_GEMM ( g ( t, x ), g ( t, g ), p ( W ), false, true );
-
-		cublasSetStream ( handle, streams[5] );
-		//carry - h state
-		CU_GEMM ( g ( t - 1, y ), g ( t, g ), p ( U ), false , true );
-
-		sync_stream ( 1 );
-		sync_stream ( 2 );
-		sync_stream ( 3 );
-		sync_stream ( 4 );
-		sync_stream ( 5 );
-	}
-
-	virtual void reset ( dtype std ) {
-
-		randn ( s ( 0, h ), ( dtype ) 0, ( dtype ) std );
-		randn ( s ( 0, c ), ( dtype ) 0, ( dtype ) std );
-		// s ( 0, h ).cu_zero();
-		// s ( 0, c ).cu_zero();
-	}
-
-	/* optional */
-	/* copy constr */
-	//hhLSTM( const hhLSTM& src) : Timelayer(src.M, src.N, src.B, src.S)  { }
-
-	/* assignent operator */
-	//hhLSTM& operator=( const hhLSTM& src) { Timelayer::operator=(src); return *this; }
-
+											N, L, s ( t, c ).rows() );
+											
+			s ( t, c ).sync_host();
+			s ( t, h ).sync_host();
+			
+		}
+		
+		virtual void backward ( bool apply_dropout, size_t t ) {
+		
+			// error coming from higher layers: dh[t] = dy[t]
+			
+			cu_elementwise_hhlstm_backward (	& ( g ( t, g ).cu_data[0] ),
+												& ( g ( t, y ).cu_data[0] ),
+												& ( s ( t, c ).cu_data[0] ),
+												& ( g ( t, c ).cu_data[0] ),
+												& ( s ( t, g ).cu_data[0] ),
+												& ( s ( t - 1, c ).cu_data[0] ),
+												& ( g ( t - 1, c ).cu_data[0] ),
+												s ( t, h ).cu_data,
+												N, L, g ( t, c ).rows() );
+												
+			//backprop through linear part (forward pass step 1)
+			//these are computed in parallel
+			cublasSetStream ( handle, streams[1] );
+			CU_GEMM ( d ( b ), p ( B_ones ), g ( t, g ), true, false );
+			
+			cublasSetStream ( handle, streams[2] );
+			CU_GEMM ( d ( U ), s ( t - 1, h ), g ( t, g ), true, false );
+			
+			cublasSetStream ( handle, streams[3] );
+			CU_GEMM ( d ( W ), s ( t, x ), g ( t, g ), true, false );
+			
+			cublasSetStream ( handle, streams[4] );
+			//backprop into inputs for lower layers
+			CU_GEMM ( g ( t, x ), g ( t, g ), p ( W ), false, true );
+			
+			cublasSetStream ( handle, streams[5] );
+			//carry - h state
+			CU_GEMM ( g ( t - 1, y ), g ( t, g ), p ( U ), false , true );
+			
+			sync_stream ( 1 );
+			sync_stream ( 2 );
+			sync_stream ( 3 );
+			sync_stream ( 4 );
+			sync_stream ( 5 );
+		}
+		
+		virtual void reset ( dtype std ) {
+		
+			randn ( s ( 0, h ), ( dtype ) 0, ( dtype ) std );
+			randn ( s ( 0, c ), ( dtype ) 0, ( dtype ) std );
+			// s ( 0, h ).cu_zero();
+			// s ( 0, c ).cu_zero();
+		}
+		
+		/* optional */
+		/* copy constr */
+		//hhLSTM( const hhLSTM& src) : Timelayer(src.M, src.N, src.B, src.S)  { }
+		
+		/* assignent operator */
+		//hhLSTM& operator=( const hhLSTM& src) { Timelayer::operator=(src); return *this; }
+		
 };
 
 #undef N
